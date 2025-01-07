39cceb38f39348a32978eb1497d325b890bb7a74
ktdinh04 <131761426+ktdinh04@users.noreply.github.com>
2025-01-07 15:33:19 +0700

    Find distance between given pairs of nodes in a binary tree

---
+++ Distance.c
@@ -0,0 +1,4 @@
+/*Find distance between given pairs of nodes in a binary tree*/
+/*Given a binary tree, determine the distance between given pairs of nodes in it. 
+The distance between two nodes is defined as the total number of edges in the shortest 
+path from one node to other.*/
